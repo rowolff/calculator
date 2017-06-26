c265a9b06203b8eda2d1bbeaa34612304c77afa0
Robert Wolff <robert.wolff@vimn.com>
2017-06-26 12:43:29 +0200

    Add display and pi, sqrt operations

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,10 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsCurrentlyTyping = false
 
     @IBAction func touchDigit(_ sender: UIButton) {
         let digit = sender.currentTitle!
-        print("\(digit) was pressed")
+        if userIsCurrentlyTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+            userIsCurrentlyTyping = true
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    @IBAction func performOpertation(_ sender: UIButton) {
+        userIsCurrentlyTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = Double.pi
+            case "√":
+                displayValue = sqrt(displayValue)  
+            default:
+                break
+            }
+        }
     }
 }
 
